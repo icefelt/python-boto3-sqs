54ac701def7f497f43969a6e62659eda2ba69a34
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-25 17:41:25 -0700

    create queue and list existing queues

--- create-sqs-queue.py
+++ create-sqs-queue.py
@@ -9,3 +9,7 @@ queue = sqs.create_queue(QueueName='test', Attributes={'DelaySeconds': '5'})
 # You can now access identifiers and attributes
 print(queue.url)
 print(queue.attributes.get('DelaySeconds'))
+
+# List all existing queues
+for queue in sqs.queues.all():
+    print(queue.url)
