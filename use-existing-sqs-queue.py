c2c878c84e7dbde1dd960716b576524596e96a03
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-25 17:44:20 -0700

    use existing sqs queue

---
+++ use-existing-sqs-queue.py
@@ -0,0 +1,11 @@
+import boto3
+
+# Get the service resource
+sqs = boto3.resource('sqs')
+
+# Get the queue. This returns an SQS.Queue instance
+queue = sqs.get_queue_by_name(QueueName='test')
+
+# You can now access identifiers and attributes
+print(queue.url)
+print(queue.attributes.get('DelaySeconds'))
